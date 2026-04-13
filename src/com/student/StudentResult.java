0d36d76f9cde8f0a044dd99064f5a539c81ade65
Nideesh-CG <nidninja10@gmail.com>
2026-04-13 16:34:14 +0530

    first commit

---
+++ src/com/student/StudentResult.java
@@ -0,0 +1,20 @@
+package com.student;
+
+public class StudentResult {
+    public static void main(String[] args) {
+        String studentName = "NIDEESH CG";
+        String registerNumber = "23MIC0092";
+        String courseName = "Computer Science";
+        String subjectName = "Application Development and Deployment Architecture";
+        int marks = 85;
+        String grade = "A";
+
+        System.out.println("Student Name: " + studentName);
+        System.out.println("Register Number: " + registerNumber);
+        System.out.println("Course Name: " + courseName);
+        System.out.println("Subject Name: " + subjectName);
+        System.out.println("Marks: " + marks);
+        System.out.println("Grade: " + grade);
+        System.out.println("Result Build Successful");
+    }
+}
