bf80bd3a8f7f98ede483abe1ecab99a0cbbe497f
Nideesh-CG <nidninja10@gmail.com>
2026-04-13 16:39:19 +0530

    Second commit

--- src/com/student/StudentResult.java
+++ src/com/student/StudentResult.java
@@ -8,13 +8,16 @@ public class StudentResult {
         String subjectName = "Application Development and Deployment Architecture";
         int marks = 85;
         String grade = "A";
+        int totalMarks = 100;
+        String resultStatus = (marks >= 40) ? "Pass" : "Fail";
 
         System.out.println("Student Name: " + studentName);
         System.out.println("Register Number: " + registerNumber);
         System.out.println("Course Name: " + courseName);
         System.out.println("Subject Name: " + subjectName);
-        System.out.println("Marks: " + marks);
+        System.out.println("Marks: " + marks + "/" + totalMarks);
         System.out.println("Grade: " + grade);
+        System.out.println("Result Status: " + resultStatus);
         System.out.println("Result Build Successful");
     }
 }
